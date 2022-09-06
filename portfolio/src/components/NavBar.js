507d4b65c2c2f765a1a87509f3744bd5cf651e38
Morgan <londonspleen@gmail.com>
2022-09-06 07:25:37 -0400

    set up components and tailwind

---
+++ portfolio/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function NavBar(){
+    return (
+        <header className="bg-pink-100">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink to="/" exact activeClassName="text-blue-400"
+                    className="inflex-flex items-center py-6 px-3 mr-4 hover:text-white cursive tracking-widest">Home</NavLink>
+                    <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded hover:text-white cursive tracking-widest">Blog</NavLink>
+                    <NavLink to="/project" className="inline-flex items-center py-3 px-3 my-6 rounded hover:text-white cursive tracking-widest">Projects</NavLink>
+                    <NavLink to="/about" className="inline-flex items-center py-3 px-3 my-6 rounded hover:text-white cursive tracking-widest">About</NavLink>
+                </nav>
+            </div>
+        </header>
+    )
+}
---
+++ portfolio/src/components/Post.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Post(){
+    return <h1>post 1 </h1>
+}
